d4c12ce5192515330e52a89d2ff16454a2af77bc
zohebh02 <zohebh02>
2024-02-11 13:08:12 +0530

    all details

--- valentine/src/app/app.component.ts
+++ valentine/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { SquareComponent } from './square/square.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SquareComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'my';
+  title = 'valentine';
 }
